f90479a03a764c20993d02ef15a761e75bc9910a
PanguPlay <purushoth.mathav@gmail.com>
2025-08-13 21:03:57 +0530

    Update mx_loader.py

--- mx_loader.py
+++ mx_loader.py
@@ -6,7 +6,7 @@ import os
 
 GITHUB_RAW_URL = "https://gist.githubusercontent.com/PurushothMathav/fcbb1f147c1670172f7466ea789e8b7d/raw/1507fe0fa730db350ac52643e1fae6e2e204b484/mx_downloader.py"
 
-print("Downloading the latest Youder source from GitHub...")
+print("Downloading the latest MX Player Content Downloader source from GitHub...")
 response = requests.get(GITHUB_RAW_URL)
 response.raise_for_status()
 
@@ -14,7 +14,8 @@ with tempfile.NamedTemporaryFile(delete=False, suffix=".py") as tmp_file:
     tmp_file.write(response.content)
     tmp_path = tmp_file.name
 
-print(f"Running Youder from: {tmp_path}")
+print(f"Running MX Player Content Downloader from: {tmp_path}")
 subprocess.run([sys.executable, tmp_path])
+
 
 os.remove(tmp_path)
